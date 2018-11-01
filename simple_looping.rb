21bb2749cdecc015dc0e31c2b3629ca646d56d72
sensible-proc-7557 <vchan313@gmail.com>
2018-11-01 14:23:42 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -22,7 +22,9 @@ end
 def times_iterator(number_of_times)
   # code your solution here using the "times" keyword
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  
+   number_of_times.times do
+    puts phrase
+  end 
 end
 
 def while_iterator(number_of_times)
@@ -36,9 +38,7 @@ def until_iterator(number_of_times)
   # code your solution here using the "until" keyword
   # hint: use a counter to tell the until loop when to stop!
   phrase = "Welcome to Flatiron School's Web Development Course!"
-  number_of_times.times do
-    puts phrase
-  end 
+ 
 
 end
 
