b0bded099750469814cec83941c9ed1516e6b1fa
sensible-proc-7557 <vchan313@gmail.com>
2018-11-01 14:21:28 +0000

    automatically backed up by learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -13,8 +13,9 @@ def loop_iterator(number_of_times)
   loop do
     puts phrase
     counter += 1
-    if counter > number_of_times
+    if counter >= number_of_times
       break
+    end
   end
 end
 
